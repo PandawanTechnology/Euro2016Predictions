14df883fe628440d95110ac3d91d79ccf606e1f3
Xavier Coureau <xavier@pandawan-technology.com>
2016-06-13 16:52:29 +0200

    Added spain/czech republic game result

--- src/AppBundle/DataFixtures/Neo4j/LoadGameData.php
+++ src/AppBundle/DataFixtures/Neo4j/LoadGameData.php
@@ -53,7 +53,7 @@ CYPHER
             [LoadTeamData::TEAM_TURQUIE, LoadTeamData::TEAM_CROATIE, '2016-06-12 15:00:00', 0, 1],
             [LoadTeamData::TEAM_POLOGNE, LoadTeamData::TEAM_IRLANDE_DU_NORD, '2016-06-12 18:00:00', 1, 0],
             [LoadTeamData::TEAM_ALLEMAGNE, LoadTeamData::TEAM_UKRAINE, '2016-06-12 21:00:00', 2, 0],
-            [LoadTeamData::TEAM_ESPAGNE, LoadTeamData::TEAM_REPUBLIQUE_TCHEQUE, '2016-06-13 15:00:00'],
+            [LoadTeamData::TEAM_ESPAGNE, LoadTeamData::TEAM_REPUBLIQUE_TCHEQUE, '2016-06-13 15:00:00', 1, 0],
             [LoadTeamData::TEAM_REPUBLIQUE_D_IRLANDE, LoadTeamData::TEAM_SUEDE, '2016-06-13 18:00:00'],
             [LoadTeamData::TEAM_BELGIQUE, LoadTeamData::TEAM_ITALIE, '2016-06-13 21:00:00'],
             [LoadTeamData::TEAM_AUTRICHE, LoadTeamData::TEAM_HONGRIE, '2016-06-14 18:00:00'],

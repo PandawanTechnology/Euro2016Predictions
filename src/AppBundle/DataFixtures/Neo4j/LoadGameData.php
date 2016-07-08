d0529ca080f4dc1104058f0e520cd6f01a3abe06
Xavier Coureau <xavier@pandawan-technology.com>
2016-07-08 03:18:16 +0200

    Added RO8, RO4 and semi finals results

--- src/AppBundle/DataFixtures/Neo4j/LoadGameData.php
+++ src/AppBundle/DataFixtures/Neo4j/LoadGameData.php
@@ -94,10 +94,17 @@ CYPHER
             [LoadTeamData::TEAM_ITALIE, LoadTeamData::TEAM_ESPAGNE, '2016-06-27 18:00:00', 2, 0],
             [LoadTeamData::TEAM_ANGLETERRE, LoadTeamData::TEAM_ISLANDE, '2016-06-27 21:00:00', 1, 2],
 
-            [LoadTeamData::TEAM_POLOGNE, LoadTeamData::TEAM_PORTUGAL, '2016-06-30 21:00:00'],
-            [LoadTeamData::TEAM_PAYS_DE_GALLE, LoadTeamData::TEAM_BELGIQUE, '2016-07-01 21:00:00'],
-            [LoadTeamData::TEAM_ALLEMAGNE, LoadTeamData::TEAM_ITALIE, '2016-07-02 21:00:00'],
-            [LoadTeamData::TEAM_FRANCE, LoadTeamData::TEAM_ISLANDE, '2016-07-03 21:00:00'],
+            // RO8
+            [LoadTeamData::TEAM_POLOGNE, LoadTeamData::TEAM_PORTUGAL, '2016-06-30 21:00:00', 1, 1],
+            [LoadTeamData::TEAM_PAYS_DE_GALLE, LoadTeamData::TEAM_BELGIQUE, '2016-07-01 21:00:00', 3, 1],
+            [LoadTeamData::TEAM_ALLEMAGNE, LoadTeamData::TEAM_ITALIE, '2016-07-02 21:00:00', 1, 1],
+            [LoadTeamData::TEAM_FRANCE, LoadTeamData::TEAM_ISLANDE, '2016-07-03 21:00:00', 5, 2],
+
+            // RO4
+            [LoadTeamData::TEAM_PORTUGAL, LoadTeamData::TEAM_PAYS_DE_GALLE, '2016-07-06 21:00:00', 2, 0],
+            [LoadTeamData::TEAM_ALLEMAGNE, LoadTeamData::TEAM_FRANCE, '2016-07-07 21:00:00', 0, 2],
+
+            [LoadTeamData::TEAM_PORTUGAL, LoadTeamData::TEAM_FRANCE, '2016-07-10 21:00:00'],
         ];
     }
 }
